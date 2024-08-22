cad0c902da537b519dc732c3d8f82faf5331fa02
Seba Fox <seba.n.fox@gmail.com>
2024-08-22 12:38:01 -0400

    This is the first part of my portfolio , VITE config, JSON files set up propperly, implementing react for faster loading, and getting the navbar setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from  "./App.module.css"
+import { Navbar } from "./components/Navbar/Navbar"
+
+
+function App() {
+  return (
+  <div className={styles.App}>
+    <Navbar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+import styles from "./Navbar.module.css";
+import { getImageUrl } from "../../utils";
+
+export const Navbar = () => {
+    return (
+         <nav className={styles.navbar}>
+            <a className={styles.title} href='/'>
+                Portfolio
+            </a>
+            <div className={styles.menu}>
+                <ul className={styles.menuItems}>
+                    <li>
+                        <a href='#about'>About</a>
+                    </li>
+                    <li>
+                        <a href='#experience'>Experience</a>
+                    </li>
+                    <li>
+                        <a href='#projects'>Projects</a>
+                    </li>
+                    <li>
+                        <a href='#contact'>Contact</a>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+};
